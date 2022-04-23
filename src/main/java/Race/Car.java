3e8c497f294bd02b8a92aac2a9c5bbf09ec5fa6f
RichardSor9e <nikitanovak91@ya.ru>
2022-04-23 12:29:41 +0300

    Initial commit

---
+++ src/main/java/Race/Car.java
@@ -0,0 +1,47 @@
+package Race;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    static {
+        CARS_COUNT = 0;
+    }
+    public static int THREAD_COUNT = 0;
+
+    private Race race;
+    private static CyclicBarrier cb = new CyclicBarrier(Main.CARS_COUNT);
+    private int speed;
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+
+            System.out.println(this.name + " готов");
+            cb.await();
+            Main.cdl1.countDown();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+
+        }
+        Main.cdl2.countDown();
+    }
+}
